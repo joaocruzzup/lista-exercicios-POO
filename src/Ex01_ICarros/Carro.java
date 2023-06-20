1a6b0014d83188ec6c7e1aae8ab4a4428a94b8c8
joaocruzzup <133882160+joaocruzzup@users.noreply.github.com>
2023-06-20 19:35:02 -0300

    :sparkles: feat: Adicionado estrutura de classes

---
+++ src/Ex01_ICarros/Carro.java
@@ -0,0 +1,4 @@
+package Ex01_ICarros;
+
+public class Carro {
+}
--- src/Ex01_ICarros/MainCarros.java
+++ src/Ex01_ICarros/MainCarros.java
@@ -1,8 +1,8 @@
-package ex01;
+package Ex01_ICarros;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class MainCarros {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
 
